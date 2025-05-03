b2ad71e7688ec59778048ce6da02141750114b3b
Mim van den Bos <mimvandenbos@gmail.com>
2025-05-03 13:01:33 +0200

    Use Quant-BnB datasets to run experiments

---
+++ README.md
@@ -0,0 +1,9 @@
+# CODT experiments
+This repository hosts the experiment setup for CODT, including comparisons against other methods.
+
+## Setup
+1. Each method is included as a git submodule, to clone all these run `git submodule update --init` after cloning. If you also want to be able to make changes in the submodules, make them track their main branch with `git submodule foreach git checkout main` and `git submodule foreach git pull origin main`
+2. Install [`uv`](https://github.com/astral-sh/uv) (faster pip replacement).
+
+## Running experiments
+Run `uv run scripts/simple.py`.
--- scripts/simple.py
+++ scripts/simple.py
@@ -1,15 +1,24 @@
 from codt_py import OptimalDecisionTreeClassifier
 from juliacall import Main as jl
+from pathlib import Path
 from pycontree import ConTree
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
+import json
 import numpy as np
-import pandas as pd
 
-df = pd.read_csv("./contree/datasets/bank.txt", sep=" ", header=None)
+SCRIPT_DIR = Path(__file__).parent.resolve()
 
-X = df[df.columns[1:]]
-y = df[0]
+def read_quant_dataset(dataset, regression = False):
+    subpath = "regress" if regression else "class"
+    y_dtype = np.float64 if regression else np.int32
+    with open(SCRIPT_DIR / f"../Quant-BnB/dataset/{subpath}/{dataset}.json") as dataset_file:
+        data = json.load(dataset_file)
+
+    return (np.array(data["Xtrain"]), np.array(data["Ytrain"], dtype=y_dtype), np.array(data["Xtest"]), np.array(data["Ytest"], dtype=y_dtype))
+
+Xtrain, Ytrain, Xtest, Ytest = read_quant_dataset("bank")
+X, y = (np.concatenate([Xtrain, Xtest]), np.concatenate([Ytrain, Ytest]))
 
 max_depth=2
 
@@ -33,4 +42,5 @@ print("CART Accuracy: " , accuracy_score(y, cart_ypred))
 jl.seval("include(\"Quant-BnB/call.jl\")")
 y_quant = np.zeros((y.size, y.max() + 1))
 y_quant[np.arange(y.size), y] = 1
-print("Quant-BnB Accuracy: ", jl.optimal_classification_2d(X.to_numpy(), y_quant))
+misses, tree = jl.optimal_classification_2d(X, y_quant)
+print("Quant-BnB Accuracy: ", 1.0 - (misses / len(y)))
